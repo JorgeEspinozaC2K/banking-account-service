fed939607f36fa0d53dbab37994f4454a4fce7c8
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-20 23:49:22 -0500

    revert changes in model

--- src/main/java/com/banking/account/service/app/model/Account.java
+++ src/main/java/com/banking/account/service/app/model/Account.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -31,6 +32,7 @@ public class Account {
 	private String password;
 	//encrypted pin
 	private String hashedpassword;
+	@Transient
 	//Account owners, titled
 	private List<String> owners;
 	private List<String> authorities;
